323efaf17796b488c91242216d0274073f471249
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-26 15:40:24 +0100

    Cache Versioning Added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
 const assets = [
   '/',
   '/index.html',
@@ -8,7 +8,9 @@ const assets = [
   '/css/styles.css',
   '/css/materialize.min.css',
   '/img/dish.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+
 ];
 
 // install service worker
@@ -26,9 +28,25 @@ self.addEventListener('install', evt => {
 // activate service worker
 self.addEventListener('activate', evt => {
   // console.log('service worker activated')
+  evt.waitUntil(
+    caches.keys()
+      .then(keys => {
+        // console.log(keys)
+        return Promise.all(keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key))
+        )
+      })
+  )
 })
 
 // tap into fetch events
 self.addEventListener('fetch', evt => {
   // console.log('fetch event')
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(cacheRes => {
+        return cacheRes || fetch(evt.request);
+      })
+  )
 })
