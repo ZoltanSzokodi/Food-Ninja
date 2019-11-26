5271ce7208ed7a7b88a6b688d23bd6affbde40c5
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-11-26 16:18:48 +0100

    Conditional Fallback

--- sw.js
+++ sw.js
@@ -58,6 +58,11 @@ self.addEventListener('fetch', evt => {
                 return fetchRes;
               })
           })
-      }).catch(() => caches.match('/pages/fallback.html'))
+      }).catch(() => {
+        if (evt.request.url.indexOf('.html') > -1) {
+          return caches.match('/pages/fallback.html');
+        }
+        // we could/should add a fallback for images too
+      })
   )
 })
